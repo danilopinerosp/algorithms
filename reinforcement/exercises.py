9331d8040cc52b39e9a7916ee8268d1ef7e7ecb0
Danilo Piñeros <dspinerosp@gmail.com>
2021-03-05 20:29:25 -0500

    Write function own_choice
    The function own_choice returns a random element from a non-empty sequence.

--- reinforcement/exercises.py
+++ reinforcement/exercises.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from random import randrange
+
 def is_multiple(n, m):
     """ Return True if n is a multiple of m, and False otherwise. """
     if n % m == 0:
@@ -35,12 +37,18 @@ def sum_squares_odd(n):
     """ Return the sum of the squares of all the odd positive integers smaller than n. """
     return sum([i * i  for i in range(n) if i % 2 != 0])
 
-if __name__ == '__main__':
-    a = [1, 3, 4, 5, -7, 4]
-    b = [5, 6, 100, 100, -6, -1000]
-    print(minmax(a))
-    print(minmax(b))
+def own_choice(data):
+    """ Return a random element from a non-empty sequence. """
+    if not len(data):
+        raise TypeError("Empty sequence.")
+    idx = randrange(0, len(data))
+    return data[idx]
 
-    for i in range(10):
-        print(sum_squares_odd(i))
+if __name__ == '__main__':
+
+    data = [1, 3, 5, 2, 7, 'hola', 'nana']
+    dat = []
+    print(own_choice(dat))
+    for i in range(5):
+        print(own_choice(data))
     
