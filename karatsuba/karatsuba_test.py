5c55752eece11daf2c9acdf34a07e320384bd3e5
Danilo Piñeros <dspinerosp@gmail.com>
2021-04-01 15:37:46 -0500

    Write three unit tests for big numbers

--- karatsuba/karatsuba_test.py
+++ karatsuba/karatsuba_test.py
@@ -20,8 +20,22 @@ class TestKaratsuba(unittest.TestCase):
     def test_multiple_digits(self):
         expected = 12040974
         self.assertEqual(karatsuba_multiplication(4698, 2563), expected)
+
     def test_one_number(self):
         expected = 0
         self.assertEqual(karatsuba_multiplication(4565), expected)
+
+    def test_large_numbers_zeros(self):
+        expected = 1003250720050501
+        self.assertEqual(karatsuba_multiplication(100020001, 10030501), expected)
+
+    def test_large_same_size(self):
+        expected = 50540736961471275
+        self.assertEqual(karatsuba_multiplication(345345345, 146348395), expected)
+
+    def test_large_different_size(self):
+        expected = 60118107496
+        self.assertEqual(karatsuba_multiplication(7655432, 7853), expected)
+        
 
 unittest.main()
