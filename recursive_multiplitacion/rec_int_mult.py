47fab3fab3890cfeaee2e94d7d0cb667f252f75a
Danilo Piñeros <dspinerosp@gmail.com>
2021-04-01 15:33:57 -0500

    Write rec_int_mult using recursive algorithm}
    The function returns the multiplication between two integer numbers, it
    doesn't matter if the are positive, negative or if they both have even or
    odd number of digits.

--- recursive_multiplitacion/rec_int_mult.py
+++ recursive_multiplitacion/rec_int_mult.py
@@ -15,27 +15,33 @@ def rec_int_mult(x, y=0):
     # sign of the final result
     sign = sign_x * sign_y
 
-    #  Numver of digits of both numers (x, y)
-    size_x = len(str(x))
-    size_y = len(str(y))
+    #  Number of digits of both numers (x, y)
 
-    # Difference between the digits of both numbers
-    delta = abs(size_x - size_y)
+    x = str(x)      # Change x to string
+    y = str(y)      # Change y to string
+    size_x = len(x)
+    size_y = len(y)
 
     n = max(size_x, size_y)
 
-    if size_x == 1 or size_y == 1:
-        return x * y
+    if n % 2 != 0 and n != 1:
+        n = n + 1
+
+    x = '0' * (n - size_x) + x
+    y = '0' * (n - size_y) + y
+        
+    if n == 1:
+        return int(x) * int(y)
     else:
-        a = x // 10 ** (size_x // 2)      # First half of x
-        b = x - a * 10 ** ( size_x // 2)  # Second half of x
-        c = y // 10 ** (size_y // 2)     # First half of y
-        d = y - c * 10 ** (size_y // 2)   # Second half of y
+        a = int(x[: n // 2])     # First half of x
+        b = int(x[n // 2:])      # Second half of x
+        c = int(y[:n // 2])     # First half of y
+        d = int(y[n // 2:])  # Second half of y
         ac = rec_int_mult(a, c)
         ad = rec_int_mult(a, d)
         bc = rec_int_mult(b, c)
         bd = rec_int_mult(b, d)
-        result = (10 ** (n - delta)) * ac + (10 ** (n // 2)) * (ad + bc) + bd
+        result = (10 ** n) * ac + (10 ** (n // 2)) * (ad + bc) + bd
         return result * sign
 
 if __name__  == '__main__':
