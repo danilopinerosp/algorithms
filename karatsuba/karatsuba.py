4f5ed99cdc062a191e1b882e694a07fdf75cee12
Danilo Piñeros <dspinerosp@gmail.com>
2021-04-01 15:58:38 -0500

    Solve bug to multiply integers with odd number of digits

--- karatsuba/karatsuba.py
+++ karatsuba/karatsuba.py
@@ -16,22 +16,28 @@ def karatsuba_multiplication(x, y = 0):
     sign = sign_x * sign_y
 
     # Number of digits of both numbers (x, y)
+    x = str(x)
+    y = str(y)
     size_x = len(str(x))
     size_y = len(str(y))
 
-    # Difference between the digits of both numbers
-    delta = abs(size_x - size_y)
-    
+
     n = max(size_x, size_y)
-    
+    if n % 2 != 0 and n != 1: # Evaluate if n not even and different to 1
+        n = n + 1             # make n even
+
+    #  Add 0's to the left to make both numbers (x, y) the same size
+    x = '0' * (n - size_x) + x 
+    y = '0' * (n - size_y) + y
+
     if size_x == 1 or size_y == 1:   #  Base case
-        return x * y
+        return int(x) * int(y)
     else:                           # Recursive case
         
-        a = x // 10 ** (size_x // 2)    # First half of x
-        b = x - a * 10 ** (size_x // 2)  # Second half of x
-        c = y // 10 ** (size_y // 2)    # First half of y
-        d = y - c * 10 ** (size_y // 2) # Second half of y
+        a = int(x[: n // 2]) # First half of x
+        b = int(x[n // 2 : ])  # Second half of x
+        c = int(y[: n // 2])    # First half of y
+        d = int(y[n // 2 :]) # Second half of y
         
         p = a + b
         q = c + d
@@ -42,7 +48,7 @@ def karatsuba_multiplication(x, y = 0):
         
         adbc = pq - ac - bd
         
-        result =  (10 ** (n - delta)) * ac + (10 ** (n // 2)) * adbc + bd
+        result =  (10 ** n) * ac + (10 ** (n // 2)) * adbc + bd
         return result * sign   # Return result with respective sign
 
 
